96b7d0666daa6c8773c4ec0950f75997821658c9
or3005 <91917236+or3005@users.noreply.github.com>
2025-10-27 10:22:42 +0200

    Update apps.py

--- games/apps.py
+++ games/apps.py
@@ -1,25 +1,30 @@
 from django.apps import AppConfig
 import threading
-
+from django.db import connection
 
 class GamesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'games'
 
     def ready(self):
-        from .models import Game
-        from .fetch_games import fetch_games_from_api
 
         def load_games():
             try:
-                if not Game.objects.exists():
-                    print("📡 Fetching games from RAWG API (on startup)...")
-                    fetch_games_from_api()
-                    print("✅ Games fetched and saved successfully!")
+
+                if 'games_game' in connection.introspection.table_names():
+                    from .models import Game
+                    from .fetch_games import fetch_games_from_api
+
+                    if not Game.objects.exists():
+                        print("📡 Fetching games from RAWG API (on startup)...")
+                        fetch_games_from_api()
+                        print("✅ Games fetched and saved successfully!")
+                    else:
+                        print("🟢 Games already exist in the database. Skipping fetch.")
                 else:
-                    print("🟢 Games already exist in the database. Skipping fetch.")
+                    print("⚠️ Table 'games_game' does not exist yet. Skipping fetch.")
             except Exception as e:
                 print(f"❌ Error fetching games: {e}")
 
-        # נשתמש ב-thread כדי לא לחסום את עליית השרת
+
         threading.Thread(target=load_games).start()
